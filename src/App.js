359ccb76d518edfeea9b7553ddb6c6f1b4dc4474
Ammar <ammarferchichi7@gmail.com>
2022-06-21 13:46:34 +0100

    packages + react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-     
+      <Header/>
+      <Router>
+        <Routes>
+        <Route path="/" element={<HomePage/>} exact />
+        <Route path="/coins/:id" element={<CoinPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
