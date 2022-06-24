6aa3f39e11c56a693382d9286275fe44863da6ea
Ammar <ammarferchichi7@gmail.com>
2022-06-24 12:08:48 +0100

    some ui interfaces

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,29 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Header.css";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "./crypto.png";
 
 
 const Header = () => {
   return (
-    <Navbar className="Nav">
-    <Container>
-      <Navbar.Brand href="#home">
-        <img
-          src="/crypto.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-        />
-      </Navbar.Brand>
-    </Container>
-  </Navbar>
+    <>
+      <Navbar className="Nav" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Cryptoo
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
 export default Header;
-=======
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
->>>>>>> 359ccb76d518edfeea9b7553ddb6c6f1b4dc4474
