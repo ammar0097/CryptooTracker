e6ec0447841d56e202b6a1cf759dd69e054be710
Ammar <ammarferchichi7@gmail.com>
2022-06-24 12:08:57 +0100

    ui

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import CoinPage from "./Pages/CoinPage";
 import Header from "./Components/Header";
+import CryptoList from "./Components/CryptoList/CryptoList";
 
-
-
-=======
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import CoinPage from "./Pages/CoinPage";
-import Header from "./Components/Header";
->>>>>>> 359ccb76d518edfeea9b7553ddb6c6f1b4dc4474
 
 function App() {
   
   return (
-<<<<<<< HEAD
     <div className="app">
       <Header />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} exact />
           <Route path="/coins/:id" element={<CoinPage />} />
-=======
-    <div className="App">
-      <Header/>
-      <Router>
-        <Routes>
-        <Route path="/" element={<HomePage/>} exact />
-        <Route path="/coins/:id" element={<CoinPage/>} />
->>>>>>> 359ccb76d518edfeea9b7553ddb6c6f1b4dc4474
         </Routes>
       </Router>
+      <CryptoList/>
     </div>
   );
 }
---
+++ src/Components/CryptoList/CryptoList.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./CryptoList.css";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+const CryptoList = () => {
+  return (
+    <Container>
+      {" "}
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default CryptoList;
